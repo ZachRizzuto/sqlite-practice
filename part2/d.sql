28d4765ea7d02390257dd2a92ac925260fb19914
Zachary Rizzuto <zachrizzuto@gmail.com>
2023-10-26 19:27:11 -0500

    solve

--- part2/d.sql
+++ part2/d.sql
@@ -1,4 +1,10 @@
 -- Below is a dummy query, if we leave it empty then the SQLITE package will throw a non-readable error
 -- Change the query to pass the test :)
-select *
-from dogs;
+
+--d:
+--Write a query to select all users' first and last names that favorited the dog with the name "Zoey"
+
+SELECT users.first_name, users.last_name FROM Users
+	JOIN Favorites 
+	ON users.id = favorites.user_id 
+	WHERE favorites.dog_id = 2;
